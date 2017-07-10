1d648383ea87e15057c6e40cd18af7a12f10658d
edon92 <540324745@qq.com>
2017-07-10 17:25:24 +0800

    商品模块

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -2,34 +2,27 @@
 * @Author: Administrator
 * @Date:   2017-06-21 15:16:24
 * @Last Modified by:   Administrator
-* @Last Modified time: 2017-06-27 15:47:26
+* @Last Modified time: 2017-07-05 23:23:44
 */
 'use strict';
-require('../module.js');
 require('./index.css');
-require('../common/nav-simple/index.js');
 require('../common/nav/index.js');
-require('../common/nav-side/index.js');
 require('page/common/header/index.js');
+require('util/slider/index.js');
+var _mm 			= require('util/mm.js');
+var templateBanner  = require('./banner.string');
+$(function(){
+	// 渲染banner的html
+	var bannerHtml = _mm.renderHtml(templateBanner);
+	$('.banner-con').html(bannerHtml);
 
-var navSide         = require('page/common/nav-side/index.js');
-var _mm = require('util/mm.js');
-navSide.init({
-	name : 'pass-update'
+	var $slider = $('.banner').unslider({
+        keys: true,
+        dots: true
+    });
+    $('.banner-arrow').click(function(){
+    	var forward = $(this).hasClass('prev') ? 'prev' : 'next';
+    	$slider.data('unslider')[forward]();
+    });
+
 });
-
-// _mm.request({
-// 	url: '/product/list.do?keyword=1',
-// 	success : function(res){
-// 		console.log(res);
-// 	},
-// 	error: function(errMsg){
-// 		console.log(errMsg);
-// 	}
-// });
-// console.log(_mm.getUrlParam('test'));
-// var html = '<div>{{data}}</div>';
-// var data = {
-// 	data :123
-// };
-// console.log(_mm.renderHtml(html,data));
